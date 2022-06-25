1b0edf54b3cedb7f7ac4ff44cd3ccd6aa4d7906f
mebiusashan <mcewei@qq.com>
2022-06-26 00:47:17 +0800

    init project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# game-develop-platform
-Game Develop Platform
+# Max2D Game Develop Platform
+
+<img align="right" width="100px" src="./logo.png">
+
+[![Go](https://github.com/max2dgame/game-develop-platform/actions/workflows/go.yml/badge.svg?branch=main)](https://github.com/max2dgame/game-develop-platform/actions/workflows/go.yml)
+
+集成式游戏开发平台
+
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	startServer()
+}
+
+func startServer() {
+	gin.SetMode(gin.ReleaseMode)
+	gin.DefaultWriter = ioutil.Discard
+
+	r := gin.Default()
+
+	//init engine
+
+	//init config
+
+	r.Static("/uploads", "./uploads")
+	r.Static("/max2d", "./client")
+	r.GET("/", func(c *gin.Context) {
+		//跳转到/luyou2对应的路由处理函数
+		c.Request.URL.Path = "/max2d" //把请求的URL修改
+		r.HandleContext(c)            //继续后续处理
+	})
+
+	//init model
+
+	_ = r.Run(":8080")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Print("closing database connection")
+	//eng.MysqlConnection().Close()
+}
